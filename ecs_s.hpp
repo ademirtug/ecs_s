af485922bb6b3374f1b48d3a1b33261a4d07409a
ademirtug <ademirtug@gmail.com>
2023-05-04 13:27:24 +0300

    typo

--- ecs_s.hpp
+++ ecs_s.hpp
@@ -4,10 +4,9 @@
 #include <memory>
 #include <unordered_map> 
 
+namespace ecs_s {
 #define _SPARSE_CAPACITY_ 8192
 
-
-namespace ecs_s {
 	using entity = uint64_t;
 	using component_id = entity;
 	class registry;
@@ -38,18 +37,7 @@ namespace ecs_s {
 		auto end();
 	};
 
-	template<typename T>
-	class system {
-	public:
-		virtual void process(registry& world, T& evt) {};
-	};
-
 	class registry {
-		uint64_t get_new_id() {
-			static uint64_t id_counter;
-			return ++id_counter;
-		}
-		template<typename T> inline T& get_component_value_for(const entity& e);
 		//term: recursive variadic helper template structure
 		template <size_t N, typename T, typename ...Ts>
 		struct component_has_helper {
@@ -64,17 +52,75 @@ namespace ecs_s {
 				return (static_cast<sparse_set<T>*>(world._component_data[world.get_component_id<T>()].get()))->has(e);
 			}
 		};
-
-		template<typename ...Ts> inline bool component_has(const entity& e);
 		std::unordered_map<component_id, std::shared_ptr<sparse_base>> _component_data;
 	public:
-		[[nodiscard]] entity new_entity();
-		void remove_entity(const entity& e);
-		template<typename T> void add_component(const entity& e, T&& p);
-		template<typename T> void remove_component(const entity& e);
-		template<typename T> [[nodiscard]] component_id get_component_id();
-		template<typename T, typename F> void each(F f);
-		template<typename... Ts, typename F> void view(F f);
+		[[nodiscard]] entity new_entity() {
+			static uint64_t entity_counter;
+			return ++entity_counter;
+		}
+
+		void remove_entity(const entity& e) {
+			for (auto it : _component_data) {
+				it.second->erase(e);
+			}
+		}
+		//registry impl
+		template<typename T>
+		inline T& get_component_value_for(const entity& e) {
+			return (*(static_cast<sparse_set<T>*>(_component_data[get_component_id<T>()].get())))[e];
+		}
+		template<typename ...Ts>
+		inline bool component_has(const entity& e) {
+			return component_has_helper<sizeof...(Ts) - 1, Ts...>::component_has_impl(*this, e);
+		}
+		//TODO: passing lvalue causes C2280! hence T&&	
+		template<typename T>
+		void add_component(const entity& e, T&& p) {
+			if (_component_data.find(get_component_id<T>()) == _component_data.end()) {
+				_component_data[get_component_id<T>()] = std::make_shared<sparse_set<T>>();
+			}
+			static_cast<sparse_set<T>*>(_component_data[get_component_id<T>()].get())->insert(e, p);
+		}
+		template<typename T>
+		void add_component(const entity& e, T& p) {
+			if (_component_data.find(get_component_id<T>()) == _component_data.end()) {
+				_component_data[get_component_id<T>()] = std::make_shared<sparse_set<T>>();
+			}
+			static_cast<sparse_set<T>*>(_component_data[get_component_id<T>()].get())->insert(e, p);
+		}
+		template<typename T>
+		void remove_component(const entity& e) {
+			static_cast<sparse_set<T>*>(_component_data[get_component_id<T>()].get())->erase(e);
+		}
+		//trick: each T will get its own static variable
+		template<typename T>
+		[[nodiscard]] component_id get_component_id() {
+			static component_id id = new_entity();
+			return id;
+		}
+		template<typename T, typename F>
+		void each(F f) {
+			sparse_set<T>* ssp = static_cast<sparse_set<T>*>(_component_data[get_component_id<T>()].get());
+			auto it = ssp->begin();
+			while (it++ != ssp->end()) {
+				f(it->sparse_index, it->payload);
+			}
+		}
+		template<typename... Ts, typename F>
+		void view(F f) {
+			using T = std::tuple_element_t<0, std::tuple<Ts...>>;
+			sparse_set<T>* ssp = static_cast<sparse_set<T>*>(_component_data[get_component_id<T>()].get());
+			if (!ssp) {
+				throw std::runtime_error("no such component!: " + std::string(typeid(T).name()));
+			}
+			for (auto it = ssp->begin(); it != ssp->end(); it++) {
+				entity e = it->sparse_index;
+				if (!component_has<Ts...>(e))
+					continue;
+				auto tx = std::move(std::make_tuple(e, get_component_value_for<Ts>(e)...));
+				std::apply(f, tx);
+			}
+		}
 	};
 
 	//sparse_set impl
@@ -111,62 +157,8 @@ namespace ecs_s {
 	}
 	template<typename T, size_t capacity>
 	auto sparse_set<T, capacity>::end() {
-		return _dense.begin() + n - 1;
+		return _dense.begin() + n;
 	}
 
-	//registry impl
-	template<typename T>
-	inline T& registry::get_component_value_for(const entity& e) {
-		return (*(static_cast<sparse_set<T>*>(_component_data[get_component_id<T>()].get())))[e];
-	}
-	template<typename ...Ts>
-	inline bool registry::component_has(const entity& e) {
-		return component_has_helper<sizeof...(Ts) - 1, Ts...>::component_has_impl(*this, e);
-	}
-	[[nodiscard]] entity registry::new_entity() {
-		static uint64_t entity_counter;
-		return ++entity_counter;
-	}
-	void registry::remove_entity(const entity& e) {
-		for (auto it : _component_data) {
-			it.second->erase(e);
-		}
-	}
-	template<typename T>
-	void registry::add_component(const entity& e, T&& p) {
-		if (_component_data.find(get_component_id<T>()) == _component_data.end()) {
-			_component_data[get_component_id<T>()] = std::make_shared<sparse_set<T>>();
-		}
-		static_cast<sparse_set<T>*>(_component_data[get_component_id<T>()].get())->insert(e, p);
-	}
-	template<typename T>
-	void registry::remove_component(const entity& e) {
-		static_cast<sparse_set<T>*>(_component_data[get_component_id<T>()].get())->erase(e);
-	}
-	//trick: each T will get its own static variable
-	template<typename T>
-	[[nodiscard]] component_id registry::get_component_id() {
-		static component_id id = get_new_id();
-		return id;
-	}
-	template<typename T, typename F>
-	void registry::each(F f) {
-		sparse_set<T>* ssp = static_cast<sparse_set<T>*>(_component_data[get_component_id<T>()].get());
-		auto it = ssp->begin();
-		while (it++ != ssp->end()) {
-			f(it->sparse_index, it->payload);
-		}
-	}
-	template<typename... Ts, typename F>
-	void registry::view(F f) {
-		using T = std::tuple_element_t<0, std::tuple<Ts...>>;
-		sparse_set<T>* ssp = static_cast<sparse_set<T>*>(_component_data[get_component_id<T>()].get());
-		for (auto it = ssp->begin(); it != ssp->end(); it++) {
-			entity e = it->sparse_index;
-			if (!component_has<Ts...>(e))
-				continue;
-			auto tx = std::move(std::make_tuple(e, get_component_value_for<Ts>(e)...));
-			std::apply(f, tx);
-		}
-	}
+
 }
